754e04843010380ec64dee7b8213b59485d6e59d
Chandan <chandank.kumar@gmail.com>
2024-03-08 21:47:02 -0500

    Update log_parsing.sh

--- log_parsing.sh
+++ log_parsing.sh
@@ -1,23 +1,14 @@
-# Log directories /var/log or any application specific log
-
-tailf or tail -f command to tail the logs while you are doing the troubleshooting
-
 tail -f syslog
-
-
-
-# Grep variations
-
-grep -i error bootstrap.log
-
-grep -rni .
-
+tail -f syslog | awk '{ print $4}'
+tail -f syslog | awk '{ print $4,$6}'
+ls
+less syslog
+tail -f syslog
+tail -f syslog | grep -i  run-containerd
 grep -i error syslog
-
 grep -i error syslog | less
-
+grep -i error syslog | awk '{ print $4}'
+grep -i error syslog | awk '{ print $4}' | less
+grep -i error syslog | less
+grep -i error syslog | awk '{ print $4,$5}' | less
 grep -i error syslog -c
-
-grep Unauthorized syslog | awk '{print $4}' | less
-
-grep Unauthorized syslog | cut -d ' ' -f4 | less
